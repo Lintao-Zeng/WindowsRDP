8712119d1849086ce26c23005aa0ee6f2ea97b4d
vermeil <2534324260@qq.com>
2024-07-10 07:07:50 +0800

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,10 +1,11 @@
 @echo off
 
-echo running
+:start
+    echo running
+    if exist C:\Users\administrator\Desktop\end.txt (
+        goto end
+    )
+    ping -n 60 127.0.0.1>nul
+goto start
 
-if exist C:\Users\administrator\Desktop\end.txt (
-    echo Found end.txt, triggering next workflow
-    curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token %GITHUB_TOKEN%" https://api.github.com/repos/%GITHUB_REPOSITORY%/actions/workflows/next-workflow.yml/dispatches -d "{\"ref\":\"main\"}"
-)
-
-ping -n 60 127.0.0.1 >nul
+:end
