e99f01a3f8487ccb395084837b79f64917cb560b
vermeil <2534324260@qq.com>
2024-07-10 06:39:12 +0800

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,10 +1,10 @@
 @echo off
 
 :start
+    echo running
     if exist C:\Users\administrator\Desktop\end.txt (
         goto end
     )
-    echo Check end
     ping -n 3 127.0.0.1>nul
     cls
 goto start
